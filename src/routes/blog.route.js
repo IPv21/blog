fe0d470127bc9175d0a500917fb6286242896dd1
will sherman <willsherman771@gmail.com>
2024-08-21 16:47:13 -0400

    patch

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -22,7 +22,36 @@ router.post('/create-post', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const posts = await Blog.find();
+        const { search, category, location } = req.query;
+        console.log(search);
+
+        let query = {};
+
+        if (search) {
+            query = {
+                ...query,
+                $or: [
+                    { title: new RegExp(search, 'i') },
+                    { content: new RegExp(search, 'i') },
+                ]
+            };
+        }
+
+        if(category) {
+            query = {
+                ...query,
+                category: category,
+            };
+        }
+
+        if(location) {
+            query = {
+                ...query,
+                location: location,
+            };
+        }
+
+        const posts = await Blog.find(query).sort({ createdAt: -1 });
         res.status(200).send({
             message: "Posts Fetched Successfully",
             posts: posts
@@ -32,5 +61,49 @@ router.get('/', async (req, res) => {
         res.status(500).send({ message: "Error Fetching Posts" });
     }
 });
+
+//get post by id
+router.get('/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        console.log(postId);
+
+        const post = await Blog.findById(postId);
+        if (!post) {
+            return res.status(404).send({ message: "Post not found" });
+        }
+
+        res.status(200).send({
+            message: "<<<>Post Fetched Successfully<>>>",
+            post: post
+        });
+    } catch (error) {
+        console.error("<<<>ERROR Fetching Post<>>>", error);
+        res.status(500).send({ message: "Error Fetching Post" });
+    }
+});
+
+//update post
+router.patch('/update-post/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        console.log("Updating post with ID:", postId);
+        console.log("Update data:", req.body);
+
+        const updatedPost = await Blog.findByIdAndUpdate(postId, req.body, { new: true, runValidators: true });
+        if (!updatedPost) {
+            return res.status(404).send({ message: "Post not found" });
+        }
+
+        res.status(200).send({
+            message: "<<<>Post Updated Successfully<>>>",
+            post: updatedPost
+        });
+    } catch (error) {
+        console.error("<<<>ERROR Updating Post<>>>", error);
+        res.status(500).send({ message: "<<<>ERROR Updating Post<>>>" });
+    }
+});
+
 
 module.exports = router;
