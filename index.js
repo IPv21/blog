0d12bd54c50066d3501052163e6a7247a2a3f8c2
will sherman <willsherman771@gmail.com>
2024-08-24 06:13:50 -0400

    before work

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const port = process .env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-// parse options
-app.use(express.json());
 app.use(cors());
-// console.log(process.env)  
-
-//routes
-const blogRoutes = require('./src/routes/blog.route')
-const commentRoutes = require('./src/routes/comment.route')
-app.use('/api/blogs', blogRoutes)
-app.use('./api/comments', commentRoutes)
 app.use(bodyParser.json());
 
-app.use('/blog', blogRoutes);
+const blogRoutes = require('./src/routes/blog.route');
+const commentRoutes = require('./src/routes/comment.route');
 
+app.use('/api/blogs', blogRoutes);
+app.use('/api/comments', commentRoutes);
 
 async function main() {
     await mongoose.connect(process.env.MONGODB_URL);
-
     app.get('/', (req, res) => {
-        res.send('Server is runninggg!!!')
-      })
-  }
+        res.send('Server is runninggg!!!');
+    });
+}
 
-
-
-  main().then(() => console.log("<<<MongoDB Connected Successfully>>>")).catch(err => console.log(err));
-
+main().then(() => console.log("<<<MongoDB Connected Successfully>>>")).catch(err => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
+const Comment = require('../model/comments.model');
 
+router.post('/create-comment', async (req, res) => {
+    try {
+        const newComment = new Comment(req.body);
+        await newComment.save();
+        res.status(201).send({
+            message: "Comment Created Successfully",
+            comment: newComment
+        });
+    } catch (error) {
+        console.error("<<<>ERROR Creating Comment<>>>", error);
+        res.status(500).send({ message: "Error Creating Comment" });
+    }
+});
 
-
-module.exports = router
+module.exports = router;
