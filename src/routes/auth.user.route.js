18227f9127ae1c050a525ea685993a1b2866388b
will sherman <willsherman771@gmail.com>
2024-08-28 20:36:28 -0400

    login and hash done

--- src/routes/auth.user.route.js
+++ src/routes/auth.user.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user.model');
+const bcrypt = require('bcryptjs');
 
 //register a new user
 router.post('/register', async(req, res) => {
@@ -19,6 +20,34 @@ router.post('/register', async(req, res) => {
     }
 
 }
+);
+
+//login a user
+router.post('/login', async(req, res) => {
+    try {
+        console.log(req.body);
+        const { email, password } = req.body;
+        const user = await  User.findOne
+        ({ email });
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send({ message: "Invalid credentials" });
+        }
+        res.status(200).send({
+            message: "Login Successful Welcome Back " + user.username + " " + user.email + " ROLE:::" + user.role
+        });
+
+        //todo generate token
+        
+
+    } catch (error) {
+        console.error("<<<>ERROR Logging In User<>>>", error);
+        res.status(500).send({ message: "Login Failed. Please try again. You were so close!" });
+    }}
+
 );
 
 
